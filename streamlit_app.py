3849befaedb1c7dba68aededa9c9e6dfc38b0515
Bao Eden Hoang <90878235+edenhng@users.noreply.github.com>
2023-06-07 11:00:36 +0930

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,33 +2,32 @@ import streamlit as st
 
 def add_logo():
     st.markdown(
-        '''
+        f"""
         <style>
-            [data-testid="stSidebarNav"] {
+            [data-testid="stSidebarNav"] + div {{
                 background-image: url(http://placekitten.com/200/200);
                 background-repeat: no-repeat;
                 padding-top: 120px;
                 background-position: 20px 20px;
-            }
-            [data-testid="stSidebarNav"]::before {
+            }}
+            [data-testid="stSidebarNav"]::before {{
                 content: "My Company Name";
                 margin-left: 20px;
                 margin-top: 20px;
                 font-size: 30px;
                 position: relative;
                 top: 100px;
-            }
+            }}
         </style>
-        ''',
+        """,
         unsafe_allow_html=True,
     )
 
 def main() :
     #Create a side bar and format it
     with st.sidebar:
-        add_logo()
         st.title("Policy at a Glance")
-        
+    add_logo()    
     #File upload function    
     uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
     for uploaded_file in uploaded_files:
