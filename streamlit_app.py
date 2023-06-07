0c95e1b2805e001be8852034c8d355c554b89d82
Bao Eden Hoang <90878235+edenhng@users.noreply.github.com>
2023-06-07 10:48:49 +0930

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,33 +1,11 @@
 import streamlit as st
 
-def add_logo():
-    st.markdown(
-        """
-        <style>
-            [data-testid="stSidebarNav"] {
-                background-image: url(http://placekitten.com/200/200);
-                background-repeat: no-repeat;
-                padding-top: 120px;
-                background-position: 20px 20px;
-            }
-            [data-testid="stSidebarNav"]::before {
-                content: "My Company Name";
-                margin-left: 20px;
-                margin-top: 20px;
-                font-size: 30px;
-                position: relative;
-                top: 100px;
-            }
-        </style>
-        """,
-        unsafe_allow_html=True,
-    )
 
 def main() :
     #Create a side bar and format it
     with st.sidebar:
         st.markdown(
-        """
+        ""
         <style>
             [data-testid="stSidebarNav"] {
                 background-image: url(http://placekitten.com/200/200);
@@ -44,7 +22,7 @@ def main() :
                 top: 100px;
             }
         </style>
-        """,
+        "",
         unsafe_allow_html=True,
     )
         st.title("Policy at a Glanceeee")
