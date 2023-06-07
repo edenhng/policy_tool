7bdff8fccb3c77b5f1944e5903bcbc9afa989f71
Bao Eden Hoang <90878235+edenhng@users.noreply.github.com>
2023-06-07 11:29:44 +0930

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,12 +7,13 @@ def main() :
         st.title("Policy at a Glance")  
     add_logo("http://placekitten.com/120/120")
     
+    st.header('A tool to mine and comprehend the policy')
+    
     #File upload function    
-    uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
-    for uploaded_file in uploaded_files:
-        bytes_data = uploaded_file.read()
-        st.write("filename:", uploaded_file.name)
-        st.write(bytes_data)
+    uploaded_file = st.file_uploader("Upload a pdf, docx or txt file")
+    bytes_data = uploaded_file.read()
+    st.write("filename:", uploaded_file.name)
+    st.write(bytes_data)
       
 if __name__ == "__main__":
     st.set_page_config(page_title="Testing Policy Tool", layout="wide")
