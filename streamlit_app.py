f62df040bae921f3aa29c094f8a7818827b84621
Bao Eden Hoang <90878235+edenhng@users.noreply.github.com>
2023-06-07 11:01:09 +0930

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,8 +26,7 @@ def add_logo():
 def main() :
     #Create a side bar and format it
     with st.sidebar:
-        st.title("Policy at a Glance")
-    add_logo()    
+        st.title("Policy at a Glance")   
     #File upload function    
     uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
     for uploaded_file in uploaded_files:
