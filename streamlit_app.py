900744e12b9cce858e301aaf0981fc38e87e6cc0
Bao Eden Hoang <90878235+edenhng@users.noreply.github.com>
2023-06-07 10:42:38 +0930

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,33 @@
 import streamlit as st
 
+def add_logo():
+    st.markdown(
+        """
+        <style>
+            [data-testid="stSidebarNav"] {
+                background-image: url(http://placekitten.com/200/200);
+                background-repeat: no-repeat;
+                padding-top: 120px;
+                background-position: 20px 20px;
+            }
+            [data-testid="stSidebarNav"]::before {
+                content: "My Company Name";
+                margin-left: 20px;
+                margin-top: 20px;
+                font-size: 30px;
+                position: relative;
+                top: 100px;
+            }
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
+
 def main() :
     #Create a side bar and format it
     with st.sidebar:
         st.title("Policy at a Glance")
+        add_logo()
         
     #File upload function    
     uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
@@ -11,7 +35,7 @@ def main() :
         bytes_data = uploaded_file.read()
         st.write("filename:", uploaded_file.name)
         st.write(bytes_data)
-        
+      
 if __name__ == "__main__":
     st.set_page_config(page_title="Testing Policy Tool", layout="wide")
     main()
