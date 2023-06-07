e4b93a2ddc93d684006a366bdb7363f28c328539
Bao Eden Hoang <90878235+edenhng@users.noreply.github.com>
2023-06-07 14:25:21 +0930

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,6 @@
 import streamlit as st
 from streamlit_extras.app_logo import add_logo
 import fitz #import PyMuPDF
-    
-def read_pdf(file):
-    with fitz.open(file) as doc:
-        doc = fitz.open(pdf_file)
-        metadata = doc.metadata
-        title = metadata['title']
-        author = metadata['author']
-        st.write("Title:", title)
-        st.write("Author:", author)
 
 def main() :
     #Create a side bar and format it
@@ -20,10 +11,16 @@ def main() :
     st.header('A tool to mine and comprehend the policy')
     
     #File upload function    
-    uploaded_file = st.file_uploader("Upload a pdf, docx or txt file")
-    doc = fitz.open(uploaded_file)
- 
-      
+    uploaded_file = st.file_uploader("Load pdf: ", type=['pdf])
+    if uploaded_pdf is not None:
+        doc = fitz.open(stream=uploaded_pdf.read(), filetype="pdf")
+        #1. Extract the Title and Author
+        metadata = doc.metadata                                               
+        title = metadata['title']
+        author = metadata['author']
+        st.write("Title:", title)
+        st.write("Author:", author)                                                        
+                      
 if __name__ == "__main__":
     st.set_page_config(page_title="Testing Policy Tool", layout="wide")
     main()
