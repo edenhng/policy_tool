39b620e7764c75a8d06c62847eca5bb0a193948c
Bao Eden Hoang <90878235+edenhng@users.noreply.github.com>
2023-06-07 15:22:23 +0930

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,26 @@ from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+def load_spacy_model():
+    nlp = spacy.load("en_core_web_sm")
+    return nlp
+
+def preprocess_text(text, nlp):
+    doc = nlp(text)
+    filtered_token = [
+        token.lemma_ for token in doc
+        if token.is_alpha and not token.is_stop and token.lemma_ not in STOP_WORDS
+    ]
+    processed_text = " ".join(filtered_tokens)
+    return processed_text
+        
+def create_word_cloud(text):
+    wordcloud = WordCloud(width=800, height = 400, max_words=100, background_color="white').generate(text)
+    plt.figure(figsize=(10, 5))
+    plt.imshow(wordcloud, interpolation ='bilinear')                    
+    plt.axis('off')
+    st.pyplot(plt)
+                          
 def main() :
     #Create a side bar and format it
     with st.sidebar:
@@ -33,7 +53,16 @@ def main() :
         else:
             st.write("Author:", author)           
         st.write("Total pages:", page_count)
-                      
+        
+        #Once the file is uploaded, convert into text and create a word cloud                  
+        text = ""
+        for page in doc:
+            text += page.getText()
+            return text
+        nlp=load_spacy_model()
+        processed_text = preprocess_text(extracted_text, nlp)
+        create_word_cloud(processed_text)                  
+    
 if __name__ == "__main__":
     st.set_page_config(page_title="Testing Policy Tool", layout="wide")
     main()
