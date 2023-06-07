82549c46abae8e692190571d676243c172de5a81
Bao Eden Hoang <90878235+edenhng@users.noreply.github.com>
2023-06-07 10:34:49 +0930

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,11 @@
 import streamlit as st
 
 def main() :
-    page = page_group("p")
-    
+    #Create a side bar and format it
     with st.sidebar:
         st.title("Policy at a Glance")
         
+    #File upload function    
     uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
     for uploaded_file in uploaded_files:
         bytes_data = uploaded_file.read()
