f9b180c4d50eafab264db23f7aba623e5464b13d
Bao Eden Hoang <90878235+edenhng@users.noreply.github.com>
2023-06-07 10:31:43 +0930

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,17 @@
 import streamlit as st
 
-from streamlit_gallery import apps, components
-from streamlit_gallery.utils.page import page_group 
-
 def main() :
     page = page_group("p")
     
     with st.sidebar:
         st.title("Policy at a Glance")
         
-    with st.expander("COMPONENTS", True):
-        page.item("Item 1 Testing")
-        page.item("Item 2 Testing")
-        page.item("Item 3 Testing")
-        page.item("Item 4 Testing")
-    
-    page.show()
-    
+    uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
+    for uploaded_file in uploaded_files:
+        bytes_data = uploaded_file.read()
+        st.write("filename:", uploaded_file.name)
+        st.write(bytes_data)
+        
 if __name__ == "__main__":
     st.set_page_config(page_title="Testing Policy Tool", layout="wide")
     main()
