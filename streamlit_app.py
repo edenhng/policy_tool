e7f8e52a3249ce6a2c51e34bba968a6d7732b29c
Bao Eden Hoang <90878235+edenhng@users.noreply.github.com>
2023-06-08 11:02:22 +0930

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,7 @@ def create_word_cloud_and_bar_chart(text):
     axes[1].set_ylabel('Frequency')
     axes[1].set_title('Top 10 Word Frequencies')
     # Rotate and align x-axis labels diagonally
-    axes[1].set_xticklabels(axes[1].get_xticklabels(), rotation=45)
+    axes[1].set_xticklabels(axes[1].get_xticklabels(), rotation=45, pad=-20)
     plt.xticks(rotation=45)
     plt.tight_layout()
     st.pyplot(fig)
