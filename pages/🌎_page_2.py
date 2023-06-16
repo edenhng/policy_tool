66a603b01354fc6ff4792463e1956f0fe766212c
Bao Eden Hoang <90878235+edenhng@users.noreply.github.com>
2023-06-16 15:16:11 +0930

    Update 🌎_page_2.py

--- pages/🌎_page_2.py
+++ pages/🌎_page_2.py
@@ -57,10 +57,7 @@ def main():
     #Retrieve the stored PDF file from the session state
     session_state = get_session_state()
     pdf_file = session_state.get('pdf_file', None) #Retrieve the 'pdf_file' attribute from session state
-    
-    if pdf_file is not None:
-        # Run LDA and display the results
-        run_lda(pdf_file)
+    run_lda(pdf_file)
 
 def get_session_state():
     if 'session_state' not in st.session_state:
