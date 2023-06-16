c01371a76dd71e8803e9fb2d857e94179f4b9475
Bao Eden Hoang <90878235+edenhng@users.noreply.github.com>
2023-06-16 15:42:41 +0930

    Update 🌎_page_2.py

--- pages/🌎_page_2.py
+++ pages/🌎_page_2.py
@@ -64,7 +64,7 @@ def main():
     session_state = get_session_state()
     pdf_file = session_state.get('pdf_file', None) #Retrieve the 'pdf_file' attribute from session state
     st.write("Check if it can work down until this way")
-    st.write(f"Type: {type(pdf_file)}"
+    st.write(f"Type: {type(pdf_file)}")
     #run_lda(pdf_file)
 
 def get_session_state():
