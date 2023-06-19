cfe8f6f123eaf2c2aa819f335ae687ca36e9f2f9
Bao Eden Hoang <90878235+edenhng@users.noreply.github.com>
2023-06-19 12:29:52 +0930

    Update 🌎_page_2.py

--- pages/🌎_page_2.py
+++ pages/🌎_page_2.py
@@ -13,9 +13,8 @@ nlp = spacy.load('en_core_web_sm')
 
 def extracted_text_from_pdf(pdf_file):
     text = ""
-    with fitz.open(working_file) as doc:
-        for page in doc:
-            text += page.get_text()
+    for page in doc:
+        text += page.get_text()
     if len(text) == 0:
         st.write("Text is empty")
     else:
