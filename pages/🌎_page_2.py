7fbd5a8473d1351912c8d254be09f54e3a63789d
Bao Eden Hoang <90878235+edenhng@users.noreply.github.com>
2023-06-19 12:03:34 +0930

    Update 🌎_page_2.py

--- pages/🌎_page_2.py
+++ pages/🌎_page_2.py
@@ -19,12 +19,12 @@ def extracted_text_from_pdf(pdf_file):
     return text
     
 def preprocess_document(pdf_file):
-    # Error handling &   # Extract text from the PDF file
     # Extract text from the PDF file
-    text = extracted_text_from_pdf(working_file)
+    text = extracted_text_from_pdf(pdf_file)
   
     # Tokenize the document
     doc = nlp(text)
+    
     # Remove stop words, punctuation, and lemmatize the tokens
     processed_tokens = []
     for token in doc:
