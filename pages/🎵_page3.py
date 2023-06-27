b22d93846b19191613f66419ddcadc56d90fb2f3
Bao Eden Hoang <90878235+edenhng@users.noreply.github.com>
2023-06-27 14:08:36 +0930

    Update 🎵_page3.py

--- pages/🎵_page3.py
+++ pages/🎵_page3.py
@@ -9,12 +9,16 @@ def highlight_entities(doc, entity_label):
     html = displacy.render(doc, style="ent", options={"ents": [entity_label]}, page=True)
     st.components.v1.html(html, height=400, scrolling=True)
 
-def extract_text_from_pdf(file):
-    doc = fitz.open(stream=file.read(), filetype="pdf")
-    text = ""
-    for page in doc:
+def extract_sentences_from_pdf(file):
+    doc = fitz.open(file)
+    sentences = []
+    nlp = spacy.load("en_core_web_sm")
+    for page_num, page in enumerate(doc, 1):
         text += page.get_text()
-    return text
+        doc = nlp(text)
+        for sent in doc.sents():
+            sentences.append((page_num, sent.text))
+    return sentences
         
 def main():
     st.title("Tabulating sentences")
@@ -22,6 +26,20 @@ def main():
     if 'working_pdf' in st.session_state:
         working_pdf = st.session_state.working_pdf
         st.info("The PDF is here")
+        sentences = extract_sentences_from_pdf(working_pdf)
+
+        if len(sentences) > 0:
+            st.write("Extracted Sentences:")
+            for page_num, sentence in sentences:
+                st.write(f"Page {page_num}: {sentence}")
+            save_csv = st.button("Save to CSV")
+            if save_csv:
+                df = pd.DataFrame(sentences, columns= ["Page Number", "Sentence"])
+                df.to_csv("extracted_sentences.csv", index = False)
+                st.write("Sentences saved to extracted_sentences.csv")
+
+                st.write("CSV File:")
+                st.write(df)
     else:
         st.warning("The PDF is not successfully uploaded. Please try again!")
     
