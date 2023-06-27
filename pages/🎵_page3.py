08fae2151e20c467f897f442fd99347a2b35b993
Bao Eden Hoang <90878235+edenhng@users.noreply.github.com>
2023-06-27 13:50:31 +0930

    Update 🎵_page3.py

--- pages/🎵_page3.py
+++ pages/🎵_page3.py
@@ -1,4 +1,3 @@
-'''
 import spacy
 import streamlit as st
 from spacy import displacy
@@ -16,28 +15,15 @@ def extract_text_from_pdf(file):
     return text
         
 def main():
-    st.title("Named Entity Recognition with spaCy")
-    file = st.file_uploader("Upload a PDF file", type="pdf")
-    entity_option = st.selectbox("Select entity to highlight", ("MONEY", "PERSON"))
-
-    if file is not None:
-        document = extract_text_from_pdf(file)
-        nlp = spacy.load("en_core_web_sm")
-        doc = nlp(document)
-        entity_label = entity_option.upper()
-
-        if st.button("Extract Sentences"):
-            entity_sentences = [sent for sent in doc.sents if any(ent.label_ == entity_label for ent in sent.ents)]
-
-            if entity_sentences:
-                st.subheader(f"Sentences with '{entity_label}' entities:")
-                for sentence in entity_sentences:
-                    st.write(sentence.text)
-                    highlight_entities(sentence, entity_label)
-                    st.write("---")
-            else:
-                st.write(f"No sentences with '{entity_label}' entities found.")
+    st.title("Tabulating sentences")
+    #Retrieve the PDF file from the session state
+    if 'working_pdf' in st.session_state:
+        working_pdf = st.session_state.working_pdf
+        st.info("The PDF is here")
+    else:
+        st.warning("The PDF is not successfully uploaded. Please try again!")
+    
 
 if __name__ == "__main__":
     main()
-'''
+
