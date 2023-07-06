f99bb1fa631cd38d9bfcfb73fb4eac6ec7808e0d
Bao Eden Hoang <90878235+edenhng@users.noreply.github.com>
2023-07-06 16:30:27 +0930

    Update 📚_page_4.py

--- pages/📚_page_4.py
+++ pages/📚_page_4.py
@@ -5,7 +5,7 @@ from transformers import pipeline
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
 def extract_text_from_pdf(file):
-    doc = fitz.open(file)
+    doc = fitz.open(stream=file.read(), filetype="pdf")
     text = ""
     for page in doc:
         text += page.get_text()
