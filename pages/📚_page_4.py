231b5233724978ac70b6e5d2b521b9909c45eebd
Bao Eden Hoang <90878235+edenhng@users.noreply.github.com>
2023-07-06 16:05:05 +0930

    Create 📚_page_4.py

---
+++ pages/📚_page_4.py
@@ -0,0 +1,85 @@
+import re
+import streamlit as st
+import fitz  # PyMuPDF
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+
+def extract_text_from_pdf(file):
+    doc = fitz.open(file)
+    text = ""
+    for page in doc:
+        text += page.getText()
+    doc.close()
+    return text
+
+def split_sentences(document):
+    sentences = re.split(r'(?<=[^A-Z].[.?]) +(?=[A-Z])', document.replace("\n", ' '))
+    return sentences
+
+def create_chunks(sentences, chunk_size):
+    chunks = []
+    current_chunk = []
+    current_length = 0
+
+    for sentence in sentences:
+        tokenized_sentence = tokenizer(sentence, truncation=False, padding=False)[0]
+        sentence_length = len(tokenized_sentence)
+
+        if current_length + sentence_length <= chunk_size:
+            current_chunk.append(sentence)
+            current_length += sentence_length
+        else:
+            chunks.append(current_chunk)
+            current_chunk = [sentence]
+            current_length = sentence_length
+
+    if current_chunk:
+        chunks.append(current_chunk)
+
+    return chunks
+
+def generate_summary(sequence, maximum_tokens, minimum_tokens):
+    input_ids = tokenizer.encode(sequence, truncation=True, max_length=1024, return_tensors="pt")
+    output = summarizer.generate(
+        input_ids,
+        num_beams=4,
+        length_penalty=2.0,
+        max_length=maximum_tokens,
+        min_length=minimum_tokens,
+        no_repeat_ngram_size=3,
+        early_stopping=True,
+        do_sample=False,
+        num_return_sequences=1
+    )
+    summary = tokenizer.decode(output[0], skip_special_tokens=True)
+    return summary
+
+# Initialize Streamlit app
+st.title("Text Summarization with BART")
+st.subheader("Upload your PDF document")
+pdf_file = st.file_uploader("Upload a PDF file", type=["pdf"])
+
+if st.button("Summarize"):
+    if pdf_file is not None:
+        input_document = extract_text_from_pdf(pdf_file)
+
+        tokenizer = AutoTokenizer.from_pretrained('facebook/bart-large-cnn')
+        summarizer = AutoModelForSeq2SeqLM.from_pretrained('facebook/bart-large-cnn')
+
+        sentences = split_sentences(input_document)
+        chunks = create_chunks(sentences, chunk_size=1024)
+
+        combined_summary = ''
+        for chunk in chunks:
+            combined_text = ' '.join(chunk)
+            summary = generate_summary(combined_text, maximum_tokens=150, minimum_tokens=40)
+            combined_summary += summary + ' '
+
+        final_summary = generate_summary(combined_summary, maximum_tokens=150, minimum_tokens=40)
+
+        st.subheader("Combined Summary:")
+        st.write(combined_summary)
+
+        st.subheader("Final Summary:")
+        st.write(final_summary)
+    else:
+        st.warning("Please upload a PDF file.")
