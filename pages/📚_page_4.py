08c4a1199d4fbc30c980b6afff3ce92a559a53dc
Bao Eden Hoang <90878235+edenhng@users.noreply.github.com>
2023-07-07 12:07:51 +0930

    Update 📚_page_4.py

--- pages/📚_page_4.py
+++ pages/📚_page_4.py
@@ -4,6 +4,12 @@ import fitz  # PyMuPDF
 from transformers import pipeline
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
+@st.cache(allow_output_mutation=True)
+def load_model():
+    tokenizer = AutoTokenizer.from_pretrained('facebook/bart-large-cnn')
+    summarizer = AutoModelForSeq2SeqLM.from_pretrained('facebook/bart-large-cnn')
+    return tokenizer, summarizer
+    
 def extract_text_from_pdf(file):
     doc = fitz.open(stream=file.read(), filetype="pdf")
     text = ""
@@ -62,9 +68,6 @@ pdf_file = st.file_uploader("Upload a PDF file", type=["pdf"])
 if st.button("Summarize"):
     if pdf_file is not None:
         input_document = extract_text_from_pdf(pdf_file)
-
-        tokenizer = AutoTokenizer.from_pretrained('facebook/bart-large-cnn')
-        summarizer = AutoModelForSeq2SeqLM.from_pretrained('facebook/bart-large-cnn')
 
         sentences = split_sentences(input_document)
         chunks = create_chunks(sentences, chunk_size=1024)
