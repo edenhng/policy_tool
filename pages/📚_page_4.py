4f179bb7aad4b95b87e189308b293a7f5935f335
Bao Eden Hoang <90878235+edenhng@users.noreply.github.com>
2023-08-15 15:50:22 +0930

    Update 📚_page_4.py

--- pages/📚_page_4.py
+++ pages/📚_page_4.py
@@ -87,7 +87,7 @@ if st.button("Summarize"):
             progress = (i + 1) / len(chunks) 
             progress_bar.progress(progress, text=progress_text)
 
-        final_summary = generate_summary(combined_summary, maximum_tokens=180, minimum_tokens=80)
+        final_summary = generate_summary(combined_summary, maximum_tokens=120, minimum_tokens=80)
         final_summary_sentences = re.split(r'(?<=[^A-Z].[.?]) +(?=[A-Z])', final_summary.replace("\n", ' '))
         bullet_points = ["• " + sentence for sentence in final_summary_sentences]
         output = "\n".join(bullet_points)
