71e6cbecf5318915ba973c4ae2f7e1663d8f1ab7
Bao Eden Hoang <90878235+edenhng@users.noreply.github.com>
2023-08-16 11:24:25 +0930

    Update home.py

--- home.py
+++ home.py
@@ -140,15 +140,15 @@ def main() :
         st.table(df)
 
         # Download button for Excel file
-        excel_buffer = io.BytesIO()
-        df.to_excel(excel_buffer, index=False)
-        excel_buffer.seek(0)  # Move the buffer cursor to the beginning
-    
-        # Create the download link for the Excel file
-        download_link = f'<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{base64.b64encode(excel_buffer.getvalue()).decode()}" download="policy_overview.xlsx">Download Excel File</a>'
+        if st.button("", key="download_button"):
+            # Save the Excel file to a BytesIO buffer
+            excel_buffer = io.BytesIO()
+            df.to_excel(excel_buffer, index=False)
+            excel_buffer.seek(0)  # Move the buffer cursor to the beginning
 
-        # Display the download link using Markdown
-        st.markdown(download_link, unsafe_allow_html=True)
+        # Create the download button using the saved Excel file
+            st.download_button(label="Download Excel File", data=excel_buffer, file_name="policy_overview.xlsx", mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+
 
         
 if __name__ == "__main__":
